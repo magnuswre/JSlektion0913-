4408c56164e8dcfeb97fb50442d9e47e0357c1ea
magnuswretblad <mwdev81@gmail.com>
2022-09-13 15:29:19 +0200

    hej

---
+++ script.js
@@ -0,0 +1,34 @@
+// // let btn1 = document.getElementById("btn1");
+// // btn1.addEventListener("click", function(){
+// // console.log('Hej')
+// // });
+
+// // SCOPE är vart man kan komma åt koden, 
+
+// // var är en global variabel, även om den är innanför local scope. accessbar överallt i koden.   
+// var hej = "hej" // GLOBAL VARIABEL
+
+// {   // HÄR ÄR LOKALT SCOPE, KALLAS BARA SCOPE 
+// let qwerty = "qweer" //LOKALA VARIBLER, KSn 
+// const asddf = " asdf" //LOKALA VARIBLER
+// }
+
+// {// HÄR ÄR ETT ANNAT LOKALT SCOPE, KALLAS BARA SCOPE 
+
+// }
+
+
+// // JAVASCRIPT TYPER
+
+
+
+// string
+// number
+// boolean
+// bigInt = jättestora nummer
+// Null = tomt, (manuellt)
+// undefined = odefinerat, tomt (automatiskt) 
+// object {}= ett objekt innehåller ett eller flera värden
+// array []= lista med värden, array är datatypen objekt 
+
+
